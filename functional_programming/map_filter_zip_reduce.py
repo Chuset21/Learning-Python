bccc3b1771798ee509578be6adb8653ab01f8736
Miguel <miguelarrieta02@gmail.com>
2021-08-02 11:38:53 +0100

    Cleaned up code with lambda expressions

--- functional_programming/map_filter_zip_reduce.py
+++ functional_programming/map_filter_zip_reduce.py
@@ -1,29 +1,17 @@
 from functools import reduce
 
 
-def mul_2(item):
-    return item * 2
-
-
-def is_odd(item):
-    return item % 2 != 0
-
-
-def accumulator(acc, item):
-    return acc + item
-
-
 def main():
     my_tuple = (1, 2, 3, 4, 5, 6)
-    your_tuple = tuple(map(mul_2, my_tuple))
+    your_tuple = tuple(map(lambda p: p * 2, my_tuple))
 
     print(your_tuple)
 
-    print(tuple(filter(is_odd, my_tuple)))
+    print(tuple(filter(lambda p: p % 2 != 0, my_tuple)))
 
     print(tuple(zip(my_tuple, your_tuple)))
 
-    print((reduce(accumulator, my_tuple, 0)))
+    print((reduce(lambda acc, p: acc + p, my_tuple, 0)))
 
 
 if __name__ == '__main__':
