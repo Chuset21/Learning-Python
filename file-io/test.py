f5a0764ed8d9afebce11539c8a804f283687b263
Miguel <miguelarrieta02@gmail.com>
2021-08-03 11:22:08 +0100

    Added try block

--- file-io/test.py
+++ file-io/test.py
@@ -1,11 +1,14 @@
 def main():
-    with open('test.txt', mode='r+') as my_file:
-        n = my_file.write('Hey, it\'s me')    # Returns the number of chars written
-        print(n)
+    try:
+        with open('test.txt', mode='r+') as my_file:
+            n = my_file.write('Hey, it\'s me')    # Returns the number of chars written
+            print(n)
 
-        # my_file.seek(0)
-        # print(my_file.readline())
-        # print(my_file.readlines())
+            # my_file.seek(0)
+            # print(my_file.readline())
+            # print(my_file.readlines())
+    except (FileNotFoundError, IOError) as err:
+        print(err)
 
 
 if __name__ == '__main__':
